fdc700cbf6f7c86e9af6b459765fbdb330a4abfb
javier <223222@ids.upchiapas.edu.mx>
2023-10-26 13:29:03 -0600

    fixed hook

--- src/app/Login/page.js
+++ src/app/Login/page.js
@@ -1,22 +1,31 @@
 'use client'
-import React from 'react'
+import React, { useState } from 'react'
 import "@/css/login.css"
 import Cuadros from '@/components/Cuadros'
 import Form_login from '@/components/form-login'
-import { useForm } from '@/hooks/useForm'
+
 import axios from 'axios'
 
 function page() {
 
-  const {emailousuario,password, onInputChange,onResetForm}=useForm({emailousuario:null,password:null})
+  // const {emailousuario,password, onInputChange,onResetForm}=useForm({emailousuario:null,password:null})
+  const [elements,setElementes]=useState({emailousuario:null,password:null})
+  const onInputChange=(e)=>{
+    const {name,value}=e.target
+    setElementes(x=>{return{
+      ...x,
+      [name]:value
+    }})
+  }
+
   const handleSubmit=async(e)=>{
     e.preventDefault()
-    console.log(password)
+    console.log(elements.password + elements.emailousuario)
  
 
 
   const res=await axios.post("https://connection-back.onrender.com/user/v1/login",{
-      usernameoremail:emailousuario,passworduser:password
+      usernameoremail:elements.emailousuario,passworduser:elements.password
     }, { withCredentials: true })
 
     console.log(res.data)
