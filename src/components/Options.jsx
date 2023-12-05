1af2f28effb02043ac01b81531f11c6b740c8ce9
javier <223222@ids.upchiapas.edu.mx>
2023-12-05 17:03:28 -0600

    nuevos cambios

--- src/components/Options.jsx
+++ src/components/Options.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "@/css/Options.css";
+import "@/css/options.css";
 import Cards from "./Cards";
 import Image from "next/image";
 import account from "@/assets/User-Account-Sign-up.svg";
